ba643c5d4f98d0a433f53aaa82c5d4caf4c3ef39
Elisa Vidal <eunice1vidal@yahoo.com>
2022-05-04 22:52:56 -0700

    tried to fix error

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -1,11 +1,27 @@
 const { Thought, User } = require('../models');
 
+const controller={
+getThoughts(req, res) {
+ Thought.find({})
+  .then(dbThoughtData => {
+      res.json(dbThoughtData)
+  })
+  .catch(err => res.status(500).json(err));
+},
+getThoughtById({ params }, res) {
+  Thought.findOne({ _id: params.id })
+  .then(dbThoughtData => {
+      if(!dbThoughtData) {
+          res.status(404).json({message: "Wrong ID"})
+      }
+      res.json(dbThoughtData);
+  })
+  .catch(err => res.status(500).json(err));
+},
+}
 
 
 
 
 
-
-
-
-module.exports = router;
+module.exports = controller;
